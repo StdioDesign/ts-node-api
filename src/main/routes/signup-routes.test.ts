9a5705182c9f998bf2cd2d1c722aa92ef59d935f
Darlan <darlan.nascimento16@gmail.com>
2021-01-28 09:05:32 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,13 +1,27 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an accout on sucess', async () => {
     await request(app)
       .post('/api/signup')
       .send({
         name: 'Darlan',
-        email: 'darla.nascimento@gmail.com',
+        email: 'darlan.nascimento@gmail.com',
         password: '123',
         passwordConfirmation: '123'
       })
