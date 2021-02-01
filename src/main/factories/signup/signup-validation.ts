27dbaabf68460e9b81cc572177cf1ddc2cebd7de
Darlan <darlan.nascimento16@gmail.com>
2021-02-01 11:52:32 -0300

    feat: add LoginValitionFactory

---
+++ src/main/factories/signup/signup-validation.ts
@@ -0,0 +1,16 @@
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-compositive'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { CompareFieldValidation } from '../../../presentation/helpers/validators/compare-field-validation'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-field-validation'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation [] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new CompareFieldValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  return new ValidationComposite(validations)
+}
