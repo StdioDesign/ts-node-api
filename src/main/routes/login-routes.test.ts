3582ec0da84b85ec6fb48213c9acd5768e7e31fd
Darlan <darlan.nascimento16@gmail.com>
2021-02-02 17:43:48 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,21 +1,25 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+beforeEach(async () => {
+  accountCollection = await MongoHelper.getCollection('accounts')
+  await accountCollection.deleteMany({})
+})
 
 describe('POST /signup', () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL)
-  })
-
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
-
-  beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
-    await accountCollection.deleteMany({})
-  })
-
   test('Should return 200 on signup', async () => {
     await request(app)
       .post('/api/signup')
@@ -24,6 +28,24 @@ describe('POST /signup', () => {
         email: 'darlan.nascimento@gmail.com',
         password: '123',
         passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
+
+describe('POST /login', () => {
+  test('Should return 200 on login', async () => {
+    const password = await hash('123', 12)
+    await accountCollection.insertOne({
+      name: 'Darlan',
+      email: 'darlan.nascimento@gmail.com',
+      password
+    })
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'darlan.nascimento@gmail.com',
+        password: '123'
       })
       .expect(200)
   })
