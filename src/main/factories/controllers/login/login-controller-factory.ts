7ed04035dce1daa9b52065832eb3a926005a50f7
Darlan <darlan.nascimento16@gmail.com>
2021-02-04 15:49:44 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -0,0 +1,10 @@
+import { makeLoginValidation } from './login-validation-factory'
+import { Controller } from '../../../../presentation/protocols'
+import { LoginController } from '../../../../presentation/controllers/Login/login-controller'
+import { makeBdAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+
+export const makeLoginController = (): Controller => {
+  const controller = new LoginController(makeBdAuthentication(), makeLoginValidation())
+  return makeLogControllerDecorator(controller)
+}
