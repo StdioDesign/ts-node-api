747bb7375225dd25301f96ff95ecaac03d10b5e0
Darlan <darlan.nascimento16@gmail.com>
2021-02-16 15:13:26 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Node API',
+    description: 'API node utilizando conceitos de TDD, SOLID e Clean Architeture',
+    version: '1.0.0'
+  }
+}
